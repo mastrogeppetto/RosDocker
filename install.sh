6763255d332651c47162abf9d5280214acc5811c
Augusto Ciuffoletti <augusto.ciuffoletti@gmail.com>
2024-11-06 18:51:23 +0100

    modifica .bashrc, aggiunte librerie grafiche mesa e git

--- install.sh
+++ install.sh
@@ -8,7 +8,6 @@ apt update
 apt install locales
 locale-gen en_US en_US.UTF-8
 update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
-export LANG=en_US.UTF-8
 apt install software-properties-common -y
 add-apt-repository universe -y
 apt update
@@ -17,5 +16,10 @@ curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 apt update
 apt install ros-jazzy-desktop -y
-apt install gedit falkon -y
+apt install gedit falkon git -y
 update-alternatives --set x-www-browser /usr/bin/falkon
+add-apt-repository ppa:kisak/kisak-mesa -y
+apt update
+apt upgrade -y
+echo "source /opt/ros/jazzy/setup.bash" >> /home/headless/.bashrc
+echo "LANG=en_US.UTF-8" >> /home/headless/.bashrc
